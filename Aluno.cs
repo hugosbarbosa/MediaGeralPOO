8db3219da38dac3595c045e35e9a138ea5434ef4
hugobarbosa <125314621+hugosbarbosa@users.noreply.github.com>
2024-12-13 20:58:48 -0300

    01

---
+++ Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+
+        private double[] _notas;
+
+        public double Media
+        {
+            get
+            {
+                return 0;
+            }
+
+
+        }
+        public Aluno(string nome, int provas)
+        {
+            Nome = nome;
+            _notas = new double[provas];
+
+        }
+
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++)
+            {
+
+            }
+        }
+    }
+}
